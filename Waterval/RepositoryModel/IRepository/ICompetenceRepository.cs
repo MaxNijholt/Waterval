0b4abba157e638ebe9e030c8915fb8671cf5c5fa
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2014-12-01 22:17:01 +0100

    Repo voor compententie aangemaakt.

---
+++ Waterval/RepositoryModel/IRepository/ICompetenceRepository.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryModel.IRepository
+{
+    public interface ICompetenceRepository
+    {
+        List<Competence> GetAll();
+        Competence Get(int compentence_id);
+        Competence Create(Competence compentence);
+        void Delete(int compentence_id);
+        Competence Update(Competence compentence);
+    }
+}
