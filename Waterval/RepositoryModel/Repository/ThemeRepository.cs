66a56dbde8e12c7d6907abece6f3b52413f0b728
Job Cuppen <jobcuppen@live.nl>
2014-12-09 15:04:27 +0100

    Afronding standalone theme crud (ONgetest)

--- Waterval/RepositoryModel/Repository/ThemeRepository.cs
+++ Waterval/RepositoryModel/Repository/ThemeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DomainModel.Models;
+using RepositoryModel.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,45 @@ using System.Threading.Tasks;
 
 namespace RepositoryModel.Repository
 {
-	public class ThemeRepository
-	{
+	public class ThemeRepository : IThemeRepository
+	{ 
+		Project_WatervalEntities dbContext;
+
+        public ThemeRepository()
+        {
+            dbContext = new DomainModel.Models.Project_WatervalEntities();
+        }
+        public List<DomainModel.Models.Theme> GetAll()
+        {
+            return dbContext.Theme.Where(b => b.isDeleted == false).ToList();
+        }
+		public DomainModel.Models.Theme Get(int theme_id)
+		{
+			return dbContext.Theme.Find(theme_id);
+		}
+		public DomainModel.Models.Theme Create(DomainModel.Models.Theme theme)
+		{
+			dbContext.Theme.Add(theme);
+			dbContext.SaveChanges();
+			return theme;
+		}
+		public DomainModel.Models.Theme Update(DomainModel.Models.Theme update)
+		{
+			Theme theme = dbContext.Theme.SingleOrDefault(b => b.Theme_ID == update.Theme_ID);
+			if(theme == null)
+				return null;
+
+			theme.Definition = update.Definition;
+			theme.Title = update.Title;
+			dbContext.SaveChanges();
+			return theme;
+		}
+		public void Delete(int theme_id)
+		{
+			Theme theme = dbContext.Theme.SingleOrDefault(c => c.Theme_ID == theme_id);
+			theme.isDeleted = true;
+			theme.DeleteDate = DateTime.UtcNow;
+			dbContext.SaveChanges();
+		}
 	}
 }
--- Waterval/Waterval/Controllers/ThemaController.cs
+++ Waterval/Waterval/Controllers/ThemaController.cs
@@ -1,36 +1,70 @@
-﻿using System;
+﻿using DomainModel.Models;
+using RepositoryModel.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MvcApplication1.Controllers
+namespace Waterval.Controllers
 {
     public class ThemaController : Controller
     {
-        //
-        // GET: /Thema/
-     
+		private ThemeRepository themeRepository;
+		public ThemaController()
+        {
+            themeRepository = new ThemeRepository();
+        }
         public ActionResult Index()
         {
-            return View();
+            return View(themeRepository.GetAll());
         }
 
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
-            return View();
+            return View(themeRepository.Get(id));
         }
 
         public ActionResult Create()
         {
-            return View();
+			Theme theme = new Theme();
+            return View(theme);
         }
+		[HttpPost]
+		public ActionResult Create(Theme theme)
+		{
+			try
+			{
+				themeRepository.Create(theme);
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View(theme);
+			}
+		}
 
-        public ActionResult Edit()
-        {
-            return View();
-        }
+		public ActionResult Edit(int id)
+		{
+			var model = themeRepository.Get(id);
+			return View(model);
+		}
 
-
+		[HttpPost]
+		public ActionResult Edit(int id, Theme theme)
+		{
+			try
+			{
+				if(themeRepository.Update(theme) == null)
+				{
+					return View(theme).ViewBag.Error = "Er is iets fout gegaan.";
+				}
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View(theme);
+			}
+		}
     }
 }
