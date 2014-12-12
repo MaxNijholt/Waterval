f8e01677de2eb770a3fb2a52101662a3356575bf
unknown <mnijholt@WIN7X64V1-052.aii.avans.nl>
2014-12-12 12:16:19 +0100

    Cherry picked the searchcontroller due to visual studio fuckups.

--- Waterval/Waterval/Controllers/HomeController.cs
+++ Waterval/Waterval/Controllers/HomeController.cs
@@ -7,10 +7,6 @@ using System.Web.Mvc;
 namespace Waterval.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Index() {
-            DomainModel.Models.Project_WatervalEntities dbContext = new DomainModel.Models.Project_WatervalEntities();
-            ViewBag.LearnLine   = new SelectList(dbContext.LearnLine, "LearnLine_ID", "Title");
-            ViewBag.Theme       = new SelectList(dbContext.Theme, "Theme_ID", "Title");
-            ViewBag.Competence  = new SelectList(dbContext.Competence, "Competence_ID", "Title");
 			return View();
 		}
 
