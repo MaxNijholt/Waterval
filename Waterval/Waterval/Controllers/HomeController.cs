391e8407bab74a8a39fda655ef79b1f3dfabf711
stanvanheumen <cjgheume@student.avans.nl>
2014-12-12 11:08:07 +0100

    Added database data to the search inputs.

--- Waterval/Waterval/Controllers/HomeController.cs
+++ Waterval/Waterval/Controllers/HomeController.cs
@@ -7,6 +7,10 @@ using System.Web.Mvc;
 namespace Waterval.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Index() {
+            DomainModel.Models.Project_WatervalEntities dbContext = new DomainModel.Models.Project_WatervalEntities();
+            ViewBag.LearnLine   = new SelectList(dbContext.LearnLine, "LearnLine_ID", "Title");
+            ViewBag.Theme       = new SelectList(dbContext.Theme, "Theme_ID", "Title");
+            ViewBag.Competence  = new SelectList(dbContext.Competence, "Competence_ID", "Title");
 			return View();
 		}
 
@@ -21,5 +25,6 @@ namespace Waterval.Controllers {
 
 			return View();
 		}
+
 	}
 }
