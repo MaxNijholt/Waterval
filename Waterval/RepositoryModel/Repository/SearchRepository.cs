36e57ab8e3f44803c7e56ce9674c3087e1c2e2a5
Max Nijholt <max@nijholt.net>
2014-12-12 14:02:59 +0100

    Removed Interface. Removed not needed functions.

--- Waterval/RepositoryModel/Repository/SearchRepository.cs
+++ Waterval/RepositoryModel/Repository/SearchRepository.cs
@@ -7,35 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RepositoryModel.Repository {
-	class SearchRepository : ISearchRepository {
+	class SearchRepository {
 
 		Project_WatervalEntities dbContext;
 
 		public SearchRepository ( ) {
 			dbContext = new DomainModel.Models.Project_WatervalEntities( );
-		}
-		public List<Module> GetAllModules ( ) {
-			return dbContext.Module.Where( b => b.isDeleted == false ).ToList( );
-		}
-
-		public Module GetModuleByID ( int module_id ) {
-			return dbContext.Module.Find( module_id );
-		}
-
-		public List<Competence> GetAllCompetences ( ) {
-			return dbContext.Competence.Where( b => b.isDeleted == false ).ToList( );
-		}
-
-		public Competence GetCompetenceByID ( int compentence_id ) {
-			return dbContext.Competence.Find( compentence_id );
-		}
-
-		public List<Block> GetAllBlocks ( ) {
-			return dbContext.Block.Where( b => b.isDeleted == false ).ToList( );
-		}
-
-		public Block GetBlockByID ( int id ) {
-			return dbContext.Block.Find( id );
 		}
 
 		public List<Block> GetBlocksWith ( String find ) {
