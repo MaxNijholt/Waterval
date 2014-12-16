1fc52241c49d2fa475244a173db918125682d8dd
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2014-12-16 01:21:23 +0100

    Unittesting start

---
+++ Waterval/UnitTests/Processors/LearnToolProcessor.cs
@@ -0,0 +1,40 @@
+﻿using DomainModel.Models;
+using RepositoryModel;
+using RepositoryModel.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Processors
+{
+    class LearnToolProcessor
+    {
+        private ILearningToolRepository learntoolRepository;
+
+        public List<LearningTool> LearningToolList;
+
+        public LearnToolProcessor(ILearningToolRepository learntoolRepository)
+        {
+            this.learntoolRepository = learntoolRepository;
+            LearningToolList = new List<LearningTool>();
+        }
+
+        public void GetAll()
+        {
+            LearningToolList = learntoolRepository.GetAll();
+        }
+
+        public void Delete()
+        {
+            learntoolRepository.Delete(4);
+            LearningToolList = learntoolRepository.GetAll();
+        }
+
+        internal void Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
