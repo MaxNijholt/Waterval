d33944511a1ff2657303cf30292abae744f38777
niekdonk <niek@student.avans.nl>
2014-12-17 11:40:14 +0100

    LearnLine index, create, edit and delete works completely. only details not finished yet

---
+++ Waterval/Waterval/Controllers/LearnLineController.cs
@@ -0,0 +1,84 @@
+﻿using DomainModel.Models;
+using RepositoryModel.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class LearnLineController : Controller
+    {
+        //
+        // GET: /LearnLine/
+        private LearnLineRepository learnLineRepository;
+
+        public LearnLineController()
+        {
+            learnLineRepository = new LearnLineRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View(learnLineRepository.GetAll());
+        }
+
+        public ActionResult Create()
+        {
+            LearnLine leer = new LearnLine();
+            return View(leer);
+        }
+
+        [HttpPost]
+        public ActionResult Create(LearnLine LearnLine)
+        {
+            try
+            {
+
+                learnLineRepository.Create(LearnLine);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(LearnLine);
+            }
+        }
+        public ActionResult Delete(LearnLine learnLine)
+        {
+
+            learnLineRepository.Delete(learnLine.LearnLine_ID);
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var model = learnLineRepository.Get(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, LearnLine learnLine)
+        {
+            try
+            {
+                if (learnLineRepository.Update(learnLine) == null)
+                {
+                    return View(learnLine).ViewBag.Error = "Er is iets fout gegaan.";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(learnLine);
+            }
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            LearnLine model = learnLineRepository.Get(id);
+            return View(model);
+        }
+
+    }
+}
