72d10e96e0c473832e3ae66c6bf70940f8890229
Emir Zeylan <aezeylan@student.avans.nl>
2014-12-17 09:54:53 +0100

    Opleiding Crud en studierichting Crud

---
+++ Waterval/RepositoryModel/Repository/ProgramRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.Models;
+using RepositoryModel.IRepository;
+
+namespace RepositoryModel.Repository
+{
+    public class ProgramRepository : IProgramRepository
+    {
+          Project_WatervalEntities dbContext;
+
+          public ProgramRepository()
+        {
+            dbContext = new DomainModel.Models.Project_WatervalEntities();
+        }
+
+          public List<DomainModel.Models.Program> GetAll()
+        {
+            return dbContext.Program.Where(b => b.isDeleted == false).ToList();
+        }
+
+        public Program Get(int id)
+        {
+            return dbContext.Program.Find(id);
+        }
+
+        public Program Create(Program program)
+        {
+            if (dbContext.Program.Any(o => o.Program_ID == program.Program_ID && !o.isDeleted))
+                return null;
+            dbContext.Program.Add(program);
+            dbContext.SaveChanges();
+            return program;
+        }
+
+        public Program Update(Program update)
+        {
+
+            Program program = dbContext.Program.SingleOrDefault(b => b.Program_ID == update.Program_ID);
+            if (program == null) return null;
+            program.Cohort = update.Cohort;
+            dbContext.SaveChanges();
+            return program;
+        }
+
+        public void Delete(int id)
+        {
+            Program program = dbContext.Program.SingleOrDefault(b => b.Program_ID == id);
+
+            program.isDeleted = true;
+            program.DeleteDate = DateTime.UtcNow;
+            dbContext.SaveChanges();
+
+        }
+    }
+}
---
+++ Waterval/Waterval/Controllers/ProgramController.cs
@@ -0,0 +1,83 @@
+﻿using DomainModel.Models;
+using RepositoryModel.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Waterval.Controllers
+{
+    public class ProgramController : Controller
+    {
+        //
+        // GET: /Program/
+        private ProgramRepository ProgramRepository;
+
+        public ProgramController()
+        {
+            ProgramRepository = new ProgramRepository();
+        }
+        public ActionResult Index()
+        {
+            return View(ProgramRepository.GetAll());
+        }
+
+        public ActionResult Details(int id)
+        {
+            Program model = ProgramRepository.Get(id);
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            Program program = new Program();
+            return View(program);
+        }
+        [HttpPost]
+        public ActionResult Create(Program program)
+        {
+            try
+            {
+                program.DeleteDate = DateTime.Now;
+                ProgramRepository.Create(program);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(program);
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            var model = ProgramRepository.Get(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Program program)
+        {
+            try
+            {
+                if (ProgramRepository.Update(program) == null)
+                {
+                    return View(program).ViewBag.Error = "Er is iets fout gegaan.";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(program);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Program program)
+        {
+
+            ProgramRepository.Delete(program.Program_ID);
+            return View();
+        }
+    }
+}
