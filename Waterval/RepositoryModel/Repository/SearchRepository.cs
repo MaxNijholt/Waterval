5cb4cc5f1b225634ef6ca7b2af419a5d80021c40
Max Nijholt <max@nijholt.net>
2014-12-18 15:14:09 +0100

    Converted to singleline

--- Waterval/RepositoryModel/Repository/SearchRepository.cs
+++ Waterval/RepositoryModel/Repository/SearchRepository.cs
@@ -17,35 +17,35 @@ namespace RepositoryModel.Repository {
 		}
 
 		public List<Block> GetBlocksWith ( String find ) {
-			List<Block> notDeleted = dbContext.Block.Where( b => b.isDeleted == false && b.Title.Contains(find) ).ToList( );
-            List<Block> returnValues = (List<Block>)( from val in notDeleted where ( val.Title.Contains( "%" + find + "%" ) ) select val ).ToList();
-            return notDeleted;
+			return dbContext.Block.Where( b => b.isDeleted == false && b.Title.Contains(find) ).ToList( );
+			//List<Block> returnValues = (List<Block>)( from val in notDeleted where ( val.Title.Contains( "%" + find + "%" ) ) select val ).ToList();
+            //return notDeleted;
 
 		}
 
 		public List<Competence> GetCompetencesWith ( String find ) {
-			List<Competence> notDeleted = dbContext.Competence.Where( b => b.isDeleted == false ).ToList( );
-			return (List<Competence>)( from val in notDeleted
-									   where ( val.Definition_Long.Contains( "%" + find + "%" )
-										   || val.Definition_Short.Contains( "%" + find + "%" )
-										   || val.Title.Contains( "%" + find + "%" ) )
-									   select val );
+			return dbContext.Competence.Where( b => b.isDeleted == false && ( b.Title.Contains(find) || b.Definition_Long.Contains(find) || b.Definition_Short.Contains(find) ) ).ToList( );
+			//return (List<Competence>)( from val in notDeleted
+			//						   where ( val.Definition_Long.Contains( "%" + find + "%" )
+			//							   || val.Definition_Short.Contains( "%" + find + "%" )
+			//							   || val.Title.Contains( "%" + find + "%" ) )
+			//						   select val );
 		}
 
 		public List<Module> GetModulesWith ( String find ) {
-			List<Module> notDeleted = dbContext.Module.Where( b => b.isDeleted == false ).ToList( );
-			return (List<Module>)( from val in notDeleted
-								   where ( val.Title.Contains( "%" + find + "%" )
-								   || val.CourseCode.Contains( "%" + find + "%" )
-								   || val.Definition_Long.Contains( "%" + find + "%" )
-								   || val.Definition_Short.Contains( "%" + find + "%" )
-								   || val.Foreknowledge.Contains( "%" + find + "%" )
-								   )
-								   select val );
+			return dbContext.Module.Where( b => b.isDeleted == false && ( b.Title.Contains(find) || b.CourseCode.Contains(find) || b.Definition_Long.Contains(find) || b.Definition_Short.Contains(find) || b.Foreknowledge.Contains(find) )).ToList( );
+			//return (List<Module>)( from val in notDeleted
+			//					   where ( val.Title.Contains( "%" + find + "%" )
+			//					   || val.CourseCode.Contains( "%" + find + "%" )
+			//					   || val.Definition_Long.Contains( "%" + find + "%" )
+			//					   || val.Definition_Short.Contains( "%" + find + "%" )
+			//					   || val.Foreknowledge.Contains( "%" + find + "%" )
+			//					   )
+			//					   select val );
 		}
 
 		public List<LearnLine> GetLearnLinesWith ( String find ) {
-            List<LearnLine> notDeleted = dbContext.LearnLine.Where(b => b.isDeleted == false && b.Title.Contains(find)).ToList();
+            return dbContext.LearnLine.Where(b => b.isDeleted == false && (b.Title.Contains(find) || b.Definition.Contains(find) )).ToList();
 
 			//List<LearnLine> notDeleted = dbContext.LearnLine.Where( b => b.isDeleted == false ).ToList( );
 		//	return (List<LearnLine>)( from val in notDeleted
@@ -53,16 +53,16 @@ namespace RepositoryModel.Repository {
 		//							  || val.Definition.Contains( "%" + find + "%" )
 		//							  )
 		//							  select val );
-            return notDeleted;
+        //    return notDeleted;
 		}
 
 		public List<Theme> GetThemesWith ( String find ) {
-			List<Theme> notDeleted = dbContext.Theme.Where( b => b.isDeleted == false ).ToList( );
-			return (List<Theme>)( from val in notDeleted
-								  where ( val.Title.Contains( "%" + find + "%" )
-								  || val.Definition.Contains( "%" + find + "%" )
-								  )
-								  select val );
+			return dbContext.Theme.Where( b => b.isDeleted == false && ( b.Title.Contains(find) || b.Definition.Contains(find))).ToList( );
+			//return (List<Theme>)( from val in notDeleted
+			//					  where ( val.Title.Contains( "%" + find + "%" )
+			//					  || val.Definition.Contains( "%" + find + "%" )
+			//					  )
+			//					  select val );
 		}
 	}
 }
