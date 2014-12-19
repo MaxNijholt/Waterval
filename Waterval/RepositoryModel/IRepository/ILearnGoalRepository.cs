3336759d773124b83fc21566ef1c4c8b1ed1fb44
GabrielWB <gwbeljaars@gmail.com>
2014-12-19 14:16:20 +0100

    Toevoegen CRUd Leerdoelen, "volgende versie" moet nog geïmplementeerd worden

---
+++ Waterval/RepositoryModel/IRepository/ILearnGoalRepository.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryModel.IRepository
+{
+    public interface ILearnGoalRepository
+    {
+        List<LearnGoal> GetAll();
+        LearnGoal Get(int learnGoal_id);
+        LearnGoal Create(LearnGoal learnGoal);
+        void Delete(int learnGoal_id);
+        LearnGoal Update(LearnGoal learnGoal);
+    }
+}
---
+++ Waterval/Waterval/Controllers/LearnGoalController.cs
@@ -0,0 +1,84 @@
+﻿using DomainModel.Models;
+using RepositoryModel.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Waterval.Controllers
+{
+    public class LearnGoalController : Controller
+    {
+        //
+        // GET: /LearnGoal/
+        private LearnGoalRepository learnGoalRepository;
+
+        public LearnGoalController()
+        {
+            learnGoalRepository = new LearnGoalRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View(learnGoalRepository.GetAll());
+        }
+
+        public ActionResult Create()
+        {
+            LearnGoal leer = new LearnGoal();
+            return View(leer);
+        }
+
+        [HttpPost]
+        public ActionResult Create(LearnGoal LearnGoal)
+        {
+            try
+            {
+
+                learnGoalRepository.Create(LearnGoal);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(LearnGoal);
+            }
+        }
+        public ActionResult Delete(LearnGoal learnGoal)
+        {
+
+            learnGoalRepository.Delete(learnGoal.LearnGoal_ID);
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var model = learnGoalRepository.Get(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, LearnGoal learnGoal)
+        {
+            try
+            {
+                if (learnGoalRepository.Update(learnGoal) == null)
+                {
+                    return View(learnGoal).ViewBag.Error = "Er is iets fout gegaan.";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(learnGoal);
+            }
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            LearnGoal model = learnGoalRepository.Get(id);
+            return View(model);
+        }
+
+    }
+}
