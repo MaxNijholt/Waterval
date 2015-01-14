2ae8d7a0d2e2c6cf3a61541ec6022bef3e4b6388
stanvanheumen <cjgheume@student.avans.nl>
2015-01-14 23:04:22 +0100

    added repos

---
+++ Waterval/RepositoryModel/IRepository/IAccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryModel.IRepository {
+
+    public interface IAccountRepository {
+
+        List<Account>   GetAll();
+        Account         Get(int accountId);
+        Account         Create(Account account);
+        Account         Update(Account account);
+        void            Delete(Account account);
+
+    }
+
+}
