42933d74bad3ce648a497ea22493eeb820cde343
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2015-01-15 14:00:27 +0100

    Untit test Competentie Done

---
+++ Waterval/RepositoryModel/DummyRepository/DummyCompetenceRepository.cs
@@ -0,0 +1,74 @@
+﻿using DomainModel.Models;
+using RepositoryModel.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryModel.DummyRepository
+{
+    public class DummyCompetenceRepository : ICompetenceRepository
+    {
+
+        private List<Competence> competenceList;
+        private List<Module> moduleList;
+        public DummyCompetenceRepository()
+        {
+            competenceList = new List<Competence>();
+            moduleList = new List<Module>();
+            CreateList();
+        }
+
+
+        public void CreateList()
+        {
+            for (int i = 1; i <= 10; i++)
+                competenceList.Add(new Competence { Competence_ID = i, Definition_Long = "Omschrijving Lang " + i, Definition_Short = "Omschrijving Kort " + i });
+
+            for (int i = 1; i <= 10; i++)
+                moduleList.Add(new Module { Module_ID = i, Definition_Long = "Omschrijving Lang " + i, Definition_Short = "Omschrijving Kort " + i });
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    competenceList.ElementAt(i - 1).Level.Add(new Level { Competence_ID = i, Level1 = (i * j).ToString(), Module_ID = i });
+                }
+            }
+        }
+
+        public List<Competence> GetAll()
+        {
+            return competenceList;
+        }
+
+        public Competence Get(int compentence_id)
+        {
+            return competenceList.Where(x => x.Competence_ID == compentence_id).First();
+        }
+
+        public Competence Create(Competence compentence)
+        {
+            competenceList.Add(compentence);
+            return compentence;
+        }
+
+        public Competence Update(Competence compentence)
+        {
+            Competence Orginal = competenceList.Where(x => x.Competence_ID == compentence.Competence_ID).First();
+
+            Orginal.Definition_Long = compentence.Definition_Long;
+
+
+            return Orginal;
+        }
+
+
+        public void Delete(int compentence_id)
+        {
+            competenceList.RemoveAll(x => x.Competence_ID == compentence_id);
+        }
+    }
+}
--- Waterval/UnitTests/Tests/CompetenceTest.cs
+++ Waterval/UnitTests/Tests/CompetenceTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DomainModel.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RepositoryModel.DummyRepository;
 using System;
 using System.Collections.Generic;
@@ -8,20 +9,99 @@ using System.Threading.Tasks;
 
 namespace UnitTests
 {
-    class CompetenceTest
+    [TestClass]
+    public class CompetenceTest
     {
+        DummyCompetenceRepository d;
 
         [TestMethod]
-        void GetAll()
+        public void GetAll()
         {
-            DModuleRepository d = new DModuleRepository();
+            d = new DummyCompetenceRepository();
 
+            List<Competence> comp = d.GetAll();
 
-List<Competence> comp =            d.GetAll();
+            int result = comp.Count;
 
-            int result = 
+            Assert.AreEqual(result, 10);
+
+        }
+        [TestMethod]
+        public void Delete()
+        {
+            d = new DummyCompetenceRepository();
+
+            List<Competence> comp = d.GetAll();
+
+            d.Delete(1);
+
+            int result = comp.Count;
+
+            Assert.AreEqual(result, 9);
+        }
+
+        [TestMethod]
+        public void GetAllLevels()
+        {
+            d = new DummyCompetenceRepository();
+
+            List<Competence> comp = d.GetAll();
+
+            int result = 0;
+
+            foreach (var item in comp)
+                foreach (var bitem in item.Level)
+                    result++;
+
+            Assert.AreEqual(result, 30);
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+
+            d = new DummyCompetenceRepository();
+            List<Competence> comp = d.GetAll();
+
+            Competence update = d.Get(3);
+
+            update.Definition_Long = "Aangepast";
+
+            d.Update(update);
+
+            Assert.AreEqual(update.Definition_Long, d.Get(3).Definition_Long);
 
         }
 
+        [TestMethod]
+        public void Create()
+        {
+            d = new DummyCompetenceRepository();
+            List<Competence> comp = d.GetAll();
+
+            d.Create(new Competence { Competence_ID = 11, Definition_Short = "Kort test", Definition_Long = "Lang Test" });
+
+            int result = comp.Count;
+
+            Assert.AreEqual(result, 11);
+        }
+
+        [TestMethod]
+        public void GetByID()
+        {
+            d = new DummyCompetenceRepository();
+            List<Competence> comp = d.GetAll();
+
+            Competence Test = new Competence();
+            Test.Competence_ID = 1;
+            Test.Definition_Short = "Omschrijving Kort 1";
+            Test.Definition_Long = "Omschrijving Lang 1";
+
+            Competence competence = d.Get(1                );
+
+            Assert.AreEqual(Test.Competence_ID,competence.Competence_ID);
+            Assert.AreEqual(Test.Definition_Short, competence.Definition_Short);
+            Assert.AreEqual(Test.Definition_Long, competence.Definition_Long);
+        }
     }
 }
