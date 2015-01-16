44a07d02536de195d35afcacc96e1d16541a4057
Job Cuppen <jobcuppen@live.nl>
2015-01-16 12:15:14 +0100

    Learngoal test added

---
+++ Waterval/RepositoryModel/DummyRepository/DummyLearnGoalRepository.cs
@@ -0,0 +1,60 @@
+﻿using DomainModel.Models;
+using RepositoryModel.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryModel.DummyRepository
+{
+    public class DummyLearnGoalRepository: ILearnGoalRepository
+    {
+        List<LearnGoal> learnGoals;
+
+        public DummyLearnGoalRepository()
+        {
+            FillList();
+        }
+
+		public List<LearnGoal> GetAll()
+        {
+            return learnGoals;
+        }
+
+		public LearnGoal Get(int learnGoal_id)
+        {
+            return learnGoals.Where(x => x.LearnGoal_ID == learnGoal_id).First();
+        }
+
+		public LearnGoal Create(LearnGoal learnGoal)
+        {
+            learnGoals.Add(learnGoal);
+            return learnGoal;
+        }
+
+        public void Delete(int learnGoal_id)
+        {
+			LearnGoal f = learnGoals.Where(x => x.LearnGoal_ID == learnGoal_id ).First();
+            f.isDeleted = true;
+            f.DeleteDate = DateTime.UtcNow;
+        }
+
+		public LearnGoal Update(LearnGoal learnGoal)
+        {
+			LearnGoal f = learnGoals.Where(x => x.LearnGoal_ID == learnGoal.LearnGoal_ID).First();
+
+            f.Description = learnGoal.Description;
+
+            return f;
+        }
+
+        private void FillList()
+        {
+			learnGoals = new List<LearnGoal>();
+
+            for(int i = 1; i <= 10; i++)
+				learnGoals.Add(new LearnGoal { LearnGoal_ID = i, Description = "LearnGoal " + i });
+        }
+    }
+}
---
+++ Waterval/UnitTests/Tests/LearnGoalTest.cs
@@ -0,0 +1,87 @@
+﻿using DomainModel.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepositoryModel.DummyRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class LearnGoalTest
+	{
+		DummyLearnGoalRepository d;
+
+		[TestMethod]
+		public void GetAll()
+		{
+			d = new DummyLearnGoalRepository();
+
+			List<LearnGoal> comp = d.GetAll();
+
+			int result = comp.Count;
+
+			Assert.AreEqual(result, 10);
+
+		}
+		[TestMethod]
+		public void Delete()
+		{
+			d = new DummyLearnGoalRepository();
+
+			List<LearnGoal> comp = d.GetAll();
+
+			d.Delete(1);
+
+			int result = comp.Count;
+
+			Assert.AreEqual(result, 9);
+		}
+		[TestMethod]
+		public void Update()
+		{
+			d = new DummyLearnGoalRepository();
+			List<LearnGoal> comp = d.GetAll();
+
+			LearnGoal update = d.Get(3);
+
+			update.Description = "Aangepast";
+
+			d.Update(update);
+
+			Assert.AreEqual(update.Description, d.Get(3).Description);
+
+		}
+
+		[TestMethod]
+		public void Create()
+		{
+			d = new DummyLearnGoalRepository();
+			List<LearnGoal> comp = d.GetAll();
+
+			d.Create(new LearnGoal { LearnGoal_ID = 11, Description = "LearnGoal" });
+
+			int result = comp.Count;
+
+			Assert.AreEqual(result, 11);
+		}
+
+		[TestMethod]
+		public void GetByID()
+		{
+			d = new DummyLearnGoalRepository();
+			List<LearnGoal> comp = d.GetAll();
+
+			LearnGoal Test = new LearnGoal();
+			Test.LearnGoal_ID = 1;
+			Test.Description = "LearnGoalNew";
+			
+			LearnGoal learnGoal = d.Get(1);
+
+			Assert.AreEqual(Test.LearnGoal_ID, learnGoal.LearnGoal_ID);
+			Assert.AreEqual(Test.Description, learnGoal.Description);
+		}
+	}
+}
